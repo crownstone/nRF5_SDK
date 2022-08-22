9f4e5db800be75ca4fe5313732a95e5791f4fbbf
Arend de Jonge <arenddejonge@gmail.com>
2022-08-22 20:34:42 +0200

    add includes for app_timer_host mock

--- components/libraries/timer/app_timer_host.cpp
+++ components/libraries/timer/app_timer_host.cpp
@@ -5,39 +5,49 @@
  * License: LGPLv3+, Apache License 2.0, and/or MIT (triple-licensed)
  */
 
+
+#include "sdk_common.h"
+//#include "timers.h"
+#include "app_timer.h"
+#include <stdlib.h>
+
+//#include "nrf.h"
+//#include "app_error.h"
+
+
 ret_code_t app_timer_init(void) {
-    return ERR_SUCCESS;
+    return NRF_SUCCESS;
 }
 
 ret_code_t app_timer_create(
     app_timer_id_t const* p_timer_id,
     app_timer_mode_t mode,
     app_timer_timeout_handler_t timeout_handler) {
-    return ERR_SUCCESS;
+    return NRF_SUCCESS;
 }
 
 ret_code_t app_timer_start(app_timer_id_t timer_id, uint32_t timeout_ticks, void* p_context) {
-    return ERR_SUCCESS;
+    return NRF_SUCCESS;
 }
 
 ret_code_t app_timer_stop(app_timer_id_t timer_id) {
-    return ERR_SUCCESS;
+    return NRF_SUCCESS;
 }
 
 ret_code_t app_timer_stop_all(void) {
-    return ERR_SUCCESS;
+    return NRF_SUCCESS;
 }
 
 uint32_t app_timer_cnt_get(void) {
-    return ERR_SUCCESS;
+    return NRF_SUCCESS;
 }
 
 uint32_t app_timer_cnt_diff_compute(uint32_t ticks_to, uint32_t ticks_from) {
-    return ERR_SUCCESS;
+    return NRF_SUCCESS;
 }
 
 uint8_t app_timer_op_queue_utilization_get(void) {
-    return ERR_SUCCESS;
+    return NRF_SUCCESS;
 }
 
 void app_timer_pause(void) {}
