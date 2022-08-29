fc10b5cb4663984ce2a04bdafbeff53d5b54b08f
Arend de Jonge <arenddejonge@gmail.com>
2022-08-29 22:44:32 +0200

    add mock file for nrf_soc.h

---
+++ components/softdevice/s132/mock/nrf_soc.cpp
@@ -0,0 +1,13 @@
+/*
+ * Author: Crownstone Team
+ * Copyright: Crownstone (https://crownstone.rocks)
+ * Date: Aug 29, 2022
+ * License: LGPLv3+, Apache License 2.0, and/or MIT (triple-licensed)
+ */
+
+
+#include <nrf_soc.h>
+
+uint32_t sd_ecb_block_encrypt(nrf_ecb_hal_data_t * p_ecb_data){
+	return NRF_SUCCESS;
+}
